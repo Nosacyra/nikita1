5cee4c0a7f3a0881d5515bed254818d36f332297
Nosacyra <49339161+Nosacyra@users.noreply.github.com>
2021-02-24 12:09:53 +0400

    маленькие исправления

--- src/stroki.java
+++ src/stroki.java
@@ -6,25 +6,30 @@ import java.io.InputStreamReader;
 
 public class stroki {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("выбирете задачу со строками");
-        System.out.println("1-самая большая");
-        System.out.println("2-средняя");
-        System.out.println("3-самая маленькая");
-        int countString = Integer.parseInt(reader.readLine());
-        switch (countString){
-            case 1:
-                Str2 a = new Str2();
-                a.Dlinna();
-            case 2:
-                Str1 b = new Str1();
-                b.Dlinna();
-            case 3:
-                Str c = new Str();
-                c.Dlinna();
+        int i = 1;
+        while (i == 1) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("выбирете задачу со строками");
+            System.out.println("1-самая большая");
+            System.out.println("2-самая маленькая");
+            int countString = Integer.parseInt(reader.readLine());
+            switch (countString) {
+                case 1:
+                    Str2 a = new Str2();
+                    a.Dlinna();
+                    i=0;
+                    break;
+                case 2:
+                    Str c = new Str();
+                    c.Dlinna();
+                    i=0;
+                    break;
+                default:
+                    System.out.println("выбирите от одного до трёх");
+
+            }
+
 
         }
-
-
     }
 }
